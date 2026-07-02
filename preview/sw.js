9c8688a44a496fd4b215179557ebd9c2f06ec952
kazezefwefew <kaanu2504@gmail.com>
2026-07-02 21:02:41 +0800

    Revert "v0.9.11.3 手机全屏按钮恢复"
    This reverts commit a0a856a7ae199d90ac18fb6c99e1873b2dc5c596.

--- preview/sw.js
+++ preview/sw.js
@@ -1,18 +1,18 @@
 "use strict";
 
 // 《逆命蛊途》PWA service worker：只预缓存当前网页试玩核心文件。
-const CACHE = "niming-pwa-v0.9.11.3";
+const CACHE = "niming-pwa-v0.9.11.2";
 const CORE = [
   "./",
   "./index.html",
   "./manifest.json",
   "./icon-192.png",
   "./icon-512.png",
-  "./style.v0998.css",
+  "./style.v0997.css",
   "./audio.v0979.js",
   "./gu_catalog.js",
   "./telemetry.v0001.js",
-  "./game.v1020.js",
+  "./game.v1019.js",
   "./version.json",
 ];
 
